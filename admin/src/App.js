603e503f9798ea7bb0355acc667b96e1c672710f
Nahid51 <mdnahidyn@gmail.com>
2022-10-27 13:43:07 +0600

    creating admin dashboard

---
+++ admin/src/App.js
@@ -0,0 +1,46 @@
+import { useContext } from "react";
+import { DarkModeContext } from "./context/darkModeContext";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import List from "./pages/List";
+import Single from "./pages/Single";
+import New from "./pages/New";
+import { productInputs, userInputs } from "./assets/formSource";
+import "./styles/dark.scss";
+
+function App() {
+
+  const { darkMode } = useContext(DarkModeContext);
+
+  return (
+    <div className={darkMode ? "app dark" : "app"}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="users">
+              <Route index element={<List />} />
+              <Route path=":userId" element={<Single />} />
+              <Route
+                path="new"
+                element={<New inputs={userInputs} title="Add New User" />}
+              />
+            </Route>
+            <Route path="products">
+              <Route index element={<List />} />
+              <Route path=":productId" element={<Single />} />
+              <Route
+                path="new"
+                element={<New inputs={productInputs} title="Add New Product" />}
+              />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
