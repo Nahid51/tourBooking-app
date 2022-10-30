6aa41bbdaee951375087beb9d7fcc27f2c148a27
Nahid51 <mdnahidyn@gmail.com>
2022-10-30 21:34:10 +0600

    added admin login system

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import List from "./pages/List";
@@ -8,32 +8,41 @@ import Single from "./pages/Single";
 import New from "./pages/New";
 import { productInputs, userInputs } from "./assets/formSource";
 import "./styles/dark.scss";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
 
   const { darkMode } = useContext(DarkModeContext);
+  const ProtectedRoute = ({ children }) => {
+    const { user } = useContext(AuthContext);
+    if (!user) {
+      return <Navigate to="/login" />
+    }
+
+    return children;
+  };
 
   return (
     <div className={darkMode ? "app dark" : "app"}>
       <BrowserRouter>
         <Routes>
           <Route path="/">
-            <Route index element={<Home />} />
             <Route path="login" element={<Login />} />
+            <Route index element={<ProtectedRoute><Home /></ProtectedRoute>} />
             <Route path="users">
-              <Route index element={<List />} />
-              <Route path=":userId" element={<Single />} />
+              <Route index element={<ProtectedRoute><List /></ProtectedRoute>} />
+              <Route path=":userId" element={<ProtectedRoute><Single /></ProtectedRoute>} />
               <Route
                 path="new"
-                element={<New inputs={userInputs} title="Add New User" />}
+                element={<ProtectedRoute><New inputs={userInputs} title="Add New User" /></ProtectedRoute>}
               />
             </Route>
             <Route path="products">
-              <Route index element={<List />} />
-              <Route path=":productId" element={<Single />} />
+              <Route index element={<ProtectedRoute><List /></ProtectedRoute>} />
+              <Route path=":productId" element={<ProtectedRoute><Single /></ProtectedRoute>} />
               <Route
                 path="new"
-                element={<New inputs={productInputs} title="Add New Product" />}
+                element={<ProtectedRoute><New inputs={productInputs} title="Add New Product" /></ProtectedRoute>}
               />
             </Route>
           </Route>
--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -1,9 +1,64 @@
-import React from 'react';
+import axios from "axios";
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+import "../styles/login.scss";
 
 const Login = () => {
+    const [userLogin, setUserLogin] = useState({
+        username: undefined,
+        password: undefined,
+    });
+
+    const { loading, error, dispatch } = useContext(AuthContext);
+
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setUserLogin((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+    };
+
+    const handleClick = async (e) => {
+        e.preventDefault();
+        dispatch({ type: "LOGIN_START" });
+        try {
+            const res = await axios.post("/auth/login", userLogin);
+            if (res.data.isAdmin) {
+                dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
+                navigate("/");
+
+            } else {
+                dispatch({ type: "LOGIN_FAILURE", payload: { message: "You are not allowed!" } });
+
+            }
+
+        } catch (error) {
+            dispatch({ type: "LOGIN_FAILURE", payload: error.response.data });
+        }
+    };
+
     return (
-        <div>
-            Login
+        <div className="login">
+            <div className="loginContainer">
+                <input
+                    type="text"
+                    placeholder="username"
+                    id="username"
+                    onChange={handleChange}
+                    className="loginInput"
+                />
+                <input
+                    type="password"
+                    placeholder="password"
+                    id="password"
+                    onChange={handleChange}
+                    className="loginInput"
+                />
+                <button disabled={loading} onClick={handleClick} className="loginButton">
+                    Login
+                </button>
+                {error && <span>{error.message}</span>}
+            </div>
         </div>
     );
 };
